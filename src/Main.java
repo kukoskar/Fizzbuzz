db3cf5166f97946446d1201849898a56b14c5afa
kukoskar <47847372+kukoskar@users.noreply.github.com>
2023-02-19 13:40:27 +0100

    update 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int number = 101;
+        for (int i = 1; i < number; i++)
+          if (i % 3 == 0 && i % 5 != 0)
+          {
+            System.out.println("Fizz");
+          }
+         else if (i % 5 == 0 && i % 3 != 0)
+          {
+            System.out.println("Buzz");
+          }
+         else if (i % 3 == 0 && i % 5 == 0)
+          {
+            System.out.println("FizzBuzz");
+          }
+         else
+            System.out.println(i);
     }
 }
